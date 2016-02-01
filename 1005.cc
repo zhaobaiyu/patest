4c1eb2d7a812640e94fe97ce7027e397b05917f9
Baiyu Zhao <csbaiyu@gmail.com>
2016-02-01 15:57:33 +0800

    1005 debug again

--- 1005.cc
+++ 1005.cc
@@ -4,11 +4,13 @@ using std::cout;
 using std::endl;
 int main() {
   int n, t[105];
-  bool a[105];
+  bool a[105], b[105];
+  for (int i = 1; i <= 100; ++i) b[i] = false;
   cin >> n;
   for (int i = 1; i <= n; ++i) a[i] = false;
   for (int i = 1; i <= n; ++i) {
     cin >> t[i];
+    b[t[i]] = true;
     int temp = t[i];
     while (temp != 1) 
       if (temp%2 == 0) {
@@ -20,10 +22,10 @@ int main() {
       }
   }
   bool space_flag = false;
-  for (int i = n; i > 0; --i) 
-    if (a[t[i]] == 0) {
+  for (int i = 100; i > 1; --i) // from high to low
+    if (a[i] == 0 && b[i] == 1) {
       if (space_flag) cout << " ";
-      cout << t[i];
+      cout << i;
       space_flag = 1;
     }
   cout << endl;
