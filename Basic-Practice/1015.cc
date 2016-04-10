529d7dc9109d67c955cf8e4a0886d889434bc7f7
Baiyu Zhao <csbaiyu@gmail.com>
2016-04-10 22:03:19 +0800

    1015 ACCEPTED!!!

--- Basic-Practice/1015.cc
+++ Basic-Practice/1015.cc
@@ -1,75 +1,75 @@
-#include <iostream>
-using std::cin;
-using std::cout;
-using std::endl;
+// It is very hard for me to solve this problem by myself.
+// I have tried many times, and eventually it is ACcepted!!!
+#include <cstdio>
+#include <cstdlib>
 typedef struct {
-  int id, de, cai, lei;
+  int id, de, cai;
+  bool is_select;
 } stu;
-void shuru(stu stux[], int stux_num, int id_tt, int de_tt, int cai_tt) {
-  stux[stux_num].id = id_tt;
-  stux[stux_num].de = de_tt;
-  stux[stux_num].cai = cai_tt;
-}
-void paixu(stu stux[], int stux_num) {
-  stu temp;
-  for (int i = 0; i < stux_num; ++i) {
-    for (int j = i + 1; j < stux_num; ++j) {
-      if (stux[i].de + stux[i].cai < stux[j].de + stux[j].cai) {
-        temp = stux[i];
-        stux[i] = stux[j];
-        stux[j] = temp;
-      }
-     else if (stux[i].de + stux[i].cai == stux[j].de + stux[j].cai && stux[i].de < stux[j].de) {
-        temp = stux[i];
-        stux[i] = stux[j];
-        stux[j] = temp;
-      }
-     else if (stux[i].de + stux[i].cai == stux[j].de + stux[j].cai && stux[i].de == stux[j].de && stux[i].id > stux[j].id) {
-        temp = stux[i];
-        stux[i] = stux[j];
-        stux[j] = temp;
-      }
-    }
-  }
-}
+stu stu1[100000];
+int no[100000];
+int cmp(const void *pa, const void *pb); //   void px(int qd, int zd);
+
 int main() {
-  int n, l, h, id_t, de_t, cai_t;
-  stu stu1[100005], stu2[100005], stu3[100005], stu4[100005];
-  int stu1_num(0), stu2_num(0), stu3_num(0), stu4_num(0);
-  cin >> n >> l >> h;
+  int n, l, h;
+  scanf("%d%d%d", &n, &l, &h);
   for (int i = 0; i < n; ++i) {
-    cin >> id_t >> de_t >> cai_t;
-    if (de_t < l || cai_t < l) continue;
-    if (de_t >= h && cai_t >= h) {
-      shuru(stu1, stu1_num, id_t, de_t, cai_t);
-      stu1_num++;
-    } else if (de_t >= h) {
-      shuru(stu2, stu2_num, id_t, de_t, cai_t);
-      stu2_num++;
-    } else if (de_t >= cai_t) {
-      shuru(stu3, stu3_num, id_t, de_t, cai_t);
-      stu3_num++;
-    } else {
-      shuru(stu4, stu4_num, id_t, de_t, cai_t);
-      stu4_num++;
+    scanf("%d%d%d", &stu1[i].id, &stu1[i].de, &stu1[i].cai);
+    if(stu1[i].de < l || stu1[i].cai < l)
+      stu1[i].is_select = true;
+    else
+      stu1[i].is_select = false;
+  }
+
+  int c1 = 0;
+  for (int i = 0; i < n; ++i) {
+    if (stu1[i].de >= h && stu1[i].cai >= h) {
+      stu1[i].is_select = true;
+      no[c1++] = i;
     }
   }
-  paixu(stu1, stu1_num);
-  paixu(stu2, stu2_num);
-  paixu(stu3, stu3_num);
-  paixu(stu4, stu4_num);
-  cout << stu1_num + stu2_num + stu3_num + stu4_num << endl;
-  for (int i = 0; i < stu1_num; ++i) {
-    cout << stu1[i].id << ' ' << stu1[i].de << ' ' << stu1[i].cai << endl;
+  qsort(&no[0], c1, sizeof(no[0]), cmp);
+  
+  int c2 = c1;
+  for (int i = 0; i < n; ++i) {
+    if (!stu1[i].is_select && stu1[i].de >= h) {
+      stu1[i].is_select = true;
+      no[c2++] = i;
+    }
   }
-  for (int i = 0; i < stu2_num; ++i) {
-    cout << stu2[i].id << ' ' << stu2[i].de << ' ' << stu2[i].cai << endl;
+  qsort(&no[c1], c2 - c1, sizeof(no[0]), cmp);  //  c2 - c1 is the sum!!! NOT c2!!!
+
+  int c3 = c2;
+  for (int i = 0; i < n; ++i) {
+    if (!stu1[i].is_select && stu1[i].cai < h && stu1[i].de >= stu1[i].cai) {
+      stu1[i].is_select = true;
+      no[c3++] = i;
+    }
   }
-  for (int i = 0; i < stu3_num; ++i) {
-    cout << stu3[i].id << ' ' << stu3[i].de << ' ' << stu3[i].cai << endl;
+  qsort(&no[c2], c3 - c2, sizeof(no[0]), cmp);
+
+  int c4 = c3;
+  for (int i = 0; i < n; ++i) {
+    if (!stu1[i].is_select) {
+      no[c4++] = i;
+    }
   }
-  for (int i = 0; i < stu4_num; ++i) {
-    cout << stu4[i].id << ' ' << stu4[i].de << ' ' << stu4[i].cai << endl;
-  }  
+  qsort(&no[c3], c4 - c3, sizeof(no[0]), cmp);
+
+  printf("%d\n", c4);
+  for (int i = 0; i < c4; ++i) {
+    printf("%d %d %d\n", stu1[no[i]].id, stu1[no[i]].de, stu1[no[i]].cai);
+  }
   return 0;
+}
+
+int cmp(const void *pa, const void *pb) {
+  int a = *(int *)pa, b = *(int *)pb;
+  if (stu1[a].de + stu1[a].cai < stu1[b].de + stu1[b].cai)
+    return 1;
+  else if (stu1[a].de + stu1[a].cai == stu1[b].de + stu1[b].cai && stu1[a].de < stu1[b].de)
+    return 1;
+  else if (stu1[a].de + stu1[a].cai == stu1[b].de + stu1[b].cai && stu1[a].de == stu1[b].de && stu1[a].id > stu1[b].id)
+    return 1;
+  else return -1;
 }
