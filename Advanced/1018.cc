5b998b892f851a1c9549740620b8629956f99b0c
Baiyu Zhao <csbaiyu@gmail.com>
2016-08-12 23:36:51 +0800

    GREAT! Advanced 1018 AC! Find all the shortest path with Dijkstra's algorithm

--- Advanced/1018.cc
+++ Advanced/1018.cc
@@ -1,3 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <climits>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::pair;
+
+vector<vector<int> > min_roads;
+vector<int> temp_road;
+vector<int> temp_road2;
+vector<vector<int> > pre;
+
+void dfs(int r, int d) { // r is the start of search, but the problem station, d is the PBMC
+  temp_road.push_back(r);
+  if (r == d) {
+    temp_road2 = temp_road;
+    std::reverse(temp_road2.begin(), temp_road2.end());
+    min_roads.push_back(temp_road2);
+    return;
+  }
+  for (auto temp : pre[r]) {
+    dfs(temp, d);
+    temp_road.pop_back();
+  }
+  return;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  int cc, n, sp, m;
+  cin >> cc >> n >> sp >> m;
+  cc /= 2;
+  vector<int> c(n+1);
+  c[0] = cc;
+  for (int i = 1; i <= n; ++i) 
+    cin >> c[i];
+  int c1, c2, t;
+  vector<vector<pair<int, int> > > road(n+1);
+  for (int i = 0; i < m; ++i) {
+    cin >> c1 >> c2 >> t;
+    road[c1].push_back({c2, t});
+    road[c2].push_back({c1, t});
+  }
+  vector<int> dist(n+1, INT_MAX);
+  dist[0] = 0;
+  vector<bool> known(n+1, false);
+  pre.assign(n+1, {});
+  int min_dist;
+  for ( ; ; ) { // dijkstra algorithm
+    min_dist = INT_MAX;
+    c1 = -1;
+    for (int i = 0; i <= n; ++i) 
+      if (!known[i] && dist[i] < min_dist) {
+        min_dist = dist[i];
+        c1 = i;
+      }
+    if (c1 == -1)
+      break;
+    known[c1] = true;
+    for (const auto &temp_pair : road[c1]) {
+      c2 = temp_pair.first;
+      t = temp_pair.second;
+      if (!known[c2]) {
+        if (dist[c1] + t < dist[c2]) {
+          dist[c2] = dist[c1] + t;
+          pre[c2] = {c1};
+        } else if (dist[c1] + t == dist[c2]) {
+          pre[c2].push_back(c1);
+        }
+      }
+    }
+  }
+  
+  dfs(sp, 0);
+
+  int diff, exist, need, min_need = INT_MAX, min_exist = INT_MAX;
+  for (const auto &v : min_roads) {
+    exist = 0;
+    need = 0;
+    for (const auto &temp : v) {
+      diff = cc - c[temp];
+      if (diff < 0) 
+        exist += -diff;
+      else if (diff > 0 && exist > diff) 
+        exist -= diff;
+      else if (diff > 0 && exist < diff) {
+        need += diff - exist;
+        exist = 0;
+      }
+    }
+    if (need < min_need || (need == min_need && exist < min_exist)) {
+      min_need = need;
+      min_exist = exist;
+      temp_road = v;
+    }
+  }
+  cout << min_need << ' ' << temp_road[0];
+  for (unsigned i = 1; i < temp_road.size(); ++i) 
+    cout << "->" << temp_road[i];
+  cout << ' ' << min_exist << endl;
+  return 0;
+}
+  
+      
+
+
+
+/*
 #include <cstdio>
 #include <vector>
 #include <utility>
@@ -91,21 +204,10 @@ int main() {
     }
   } // main for loop
 
-  // test
-  /*
-  temp_i = 0;
-  for (const auto &vex_temp : vex) {
-    printf("#%d need: %d known: %d dist: %d pre: %d bike: %d max_bike: %d\n", temp_i++, vex_temp.need, vex_temp.known, vex_temp.dist, vex_temp.pre, vex_temp.bike, vex_temp.max_bike);
-    for (const auto &vex_edge_test : vex_temp.edge) {
-      printf("\tto %d, time: %d\n", vex_edge_test.first, vex_edge_test.second);
-    }
-  }
-  */
-  // test
-
   printf("%d ", vex[sp].max_bike);
   PrintPath(sp, vex);
   printf(" %d\n", vex[sp].max_bike - vex[sp].bike);
 
   return 0;
 }
+*/
